fe750e5c4d2f73a78486d7b73cfb3103175076ea
Варвара Бажукова <mamasetta@gmail.com>
2020-07-06 23:03:35 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,44 +1,46 @@
 'use strict';
 
-var picturesElement = document.querySelector('.pictures');
-var bigPicture = document.querySelector('.big-picture');
-var bigPictureCansel = bigPicture.querySelector('.big-picture__cancel');
-var uploadFile = document.querySelector('#upload-file');
-var uploadCancel = document.querySelector('#upload-cancel');
+(function () {
+  var picturesElement = document.querySelector('.pictures');
+  var bigPicture = document.querySelector('.big-picture');
+  var bigPictureCansel = bigPicture.querySelector('.big-picture__cancel');
+  var uploadFile = document.querySelector('#upload-file');
+  var uploadCancel = document.querySelector('#upload-cancel');
 
-window.usersPhotos.renderPictures();
-window.usersComments.renderComments();
+  window.usersPhotos.renderPictures();
+  window.usersComments.renderComments();
 
-picturesElement.addEventListener('click', function (evt) {
-  if (evt.target.classList.contains('picture__img')) {
+  picturesElement.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('picture__img')) {
+      evt.preventDefault();
+      window.bigPicture.openUsersPicture(Number(evt.target.closest('.picture').dataset.id));
+    }
+  });
+
+  picturesElement.addEventListener('keydown', function (evt) {
+    if (evt.key === window.utils.Key.ENTER && evt.target.classList.contains('picture')) {
+      evt.preventDefault();
+      window.bigPicture.openUsersPicture(Number(evt.target.dataset.id));
+    }
+  });
+
+  bigPictureCansel.addEventListener('click', function (evt) {
     evt.preventDefault();
-    window.bigPicture.openUsersPicture(Number(evt.target.closest('.picture').dataset.id));
-  }
-});
+    window.bigPicture.closeUsersPicture();
+  });
 
-picturesElement.addEventListener('keydown', function (evt) {
-  if (evt.key === window.utils.Key.ENTER && evt.target.classList.contains('picture')) {
+  uploadFile.addEventListener('change', function (evt) {
     evt.preventDefault();
-    window.bigPicture.openUsersPicture(Number(evt.target.dataset.id));
-  }
-});
+    window.form.openUploadFile();
+  });
 
-bigPictureCansel.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  window.bigPicture.closeUsersPicture();
-});
-
-uploadFile.addEventListener('change', function (evt) {
-  evt.preventDefault();
-  window.form.openUploadFile();
-});
-
-uploadCancel.addEventListener('click', function () {
-  window.form.closeUploadFile();
-});
-
-uploadCancel.addEventListener('keydown', function (evt) {
-  if (evt.key === window.utils.Key.ENTER) {
+  uploadCancel.addEventListener('click', function () {
     window.form.closeUploadFile();
-  }
-});
+  });
+
+  uploadCancel.addEventListener('keydown', function (evt) {
+    if (evt.key === window.utils.Key.ENTER) {
+      window.form.closeUploadFile();
+    }
+  });
+})();
