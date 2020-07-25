399fa93c93514316039646964d65d6c3d6cbe1dc
Варвара Бажукова <mamasetta@gmail.com>
2020-07-25 19:28:41 +0500

    load photo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 (function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
   var picturesElement = document.querySelector('.pictures');
   var bigPicture = document.querySelector('.big-picture');
   var bigPictureCansel = bigPicture.querySelector('.big-picture__cancel');
   var imageUploadForm = document.querySelector('.img-upload__form');
+  var imageUploadInput = document.querySelector('.img-upload__input');
+  var imageUploadPreview = document.querySelector('.img-upload__preview img');
+  var effectsPreview = document.querySelector('.effects__preview');
   var uploadFile = document.querySelector('#upload-file');
   var uploadCancel = document.querySelector('#upload-cancel');
   var uploadForm = document.querySelector('#upload-select-image');
@@ -57,6 +62,24 @@
 
   uploadFile.addEventListener('change', function (evt) {
     evt.preventDefault();
+
+    var file = imageUploadInput.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        imageUploadPreview.src = reader.result;
+        effectsPreview.style.backgroundImage = 'url(' + reader.result + ')';
+      });
+
+      reader.readAsDataURL(file);
+    }
     window.form.openUploadFile();
   });
 
